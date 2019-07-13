36b7dbdd7dd31441e56b4f9a5f55e404bf92962f
gilann <mad.shivam111@gmail.com>
2019-07-13 20:04:43 +0530

    add password field in user table

--- loop.sql
+++ loop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2019 at 02:33 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Jul 13, 2019 at 04:33 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -118,6 +118,7 @@ CREATE TABLE `schools` (
 
 CREATE TABLE `users` (
   `uid` int(11) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `uname` varchar(200) NOT NULL,
   `phone` int(10) NOT NULL,
   `email` varchar(200) NOT NULL,
