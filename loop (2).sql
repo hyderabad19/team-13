35c38f79c7981e2b065d5e865096f56d03099d75
gowtham181998 <47582807+gowtham181998@users.noreply.github.com>
2019-07-13 23:11:39 +0530

    added cluster table

---
+++ loop (2).sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2019 at 07:39 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `available`
+--
+
+CREATE TABLE `available` (
+  `aid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
+  `fromtime` datetime NOT NULL,
+  `totime` datetime NOT NULL,
+  `isavailable` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `bid` int(11) NOT NULL,
+  `usedby` int(11) NOT NULL,
+  `providedby` int(11) NOT NULL,
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `fid` int(11) NOT NULL,
+  `bid` int(11) NOT NULL,
+  `user` varchar(500) NOT NULL,
+  `provider` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `human_resources`
+--
+
+CREATE TABLE `human_resources` (
+  `rid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `tname` varchar(200) NOT NULL,
+  `subject` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `material_resource`
+--
+
+CREATE TABLE `material_resource` (
+  `rid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `type` varchar(200) NOT NULL,
+  `capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schools`
+--
+
+CREATE TABLE `schools` (
+  `sid` int(11) NOT NULL,
+  `sname` varchar(200) NOT NULL,
+  `state` varchar(200) NOT NULL,
+  `city` varchar(200) NOT NULL,
+  `mandal` varchar(200) NOT NULL,
+  `cluster` varchar(200) NOT NULL,
+  `pincode` int(6) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `dise` int(11) NOT NULL,
+  `scategory` varchar(200) NOT NULL,
+  `smanagement` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `uname` varchar(200) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `sid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `available`
+--
+ALTER TABLE `available`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`bid`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`fid`);
+
+--
+-- Indexes for table `human_resources`
+--
+ALTER TABLE `human_resources`
+  ADD PRIMARY KEY (`rid`);
+
+--
+-- Indexes for table `material_resource`
+--
+ALTER TABLE `material_resource`
+  ADD PRIMARY KEY (`rid`);
+
+--
+-- Indexes for table `schools`
+--
+ALTER TABLE `schools`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `sid` (`sid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `available`
+--
+ALTER TABLE `available`
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `human_resources`
+--
+ALTER TABLE `human_resources`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `schools`
+--
+ALTER TABLE `schools`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
