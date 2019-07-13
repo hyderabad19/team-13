90339aed9a0522c44d2fecb8217f7aa17a9c433a
gilann <mad.shivam111@gmail.com>
2019-07-14 02:32:59 +0530

    added addCluster

---
+++ addedCluster.php
@@ -0,0 +1,24 @@
+<?php
+$cname = $_POST["name"];
+$mandal = $_POST["mandal"];
+$state = $_POST["state"];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "loop";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+		
+$sql = "insert into clusters(cname, mandal, state) 
+VALUES ('$cname', '$mandal', '$state')";
+
+if ($conn->query($sql)) {
+    header("Location:addCluster.php");
+}
+?>
