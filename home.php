b47815853ecc78bc8775349931efb66cd929b7d1
gilann <mad.shivam111@gmail.com>
2019-07-14 09:34:39 +0530

    add home page

---
+++ home.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html>
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styletemp.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container-main-wrapper">
+		<header class="header-wrapper">
+			<div class="container">
+				<div class="header-menu-wrapper" >
+					<div class="brand-logo">
+						<img class="img-responsive" src="images/loop.png" alt="image">
+					</div>
+					<div class="main-menu">
+						<ul class="nav navbar-nav ">
+							<li ><a href="home.php">Home</a></li>
+							<li ><a href="login.php">Login</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="content-main-wrapper">
+			<div class="w3-content w3-section" style="max-width:500px"></div>
+			<h1 align=center>education is right</h1>
+			<img  src="images/img1.png" style="width:100%" >
+			<div class="content-main-wrapper1">
+			<div class="container1">
+			<div class="content-wrapper1">
+					<div class="gallery">
+						<img src="images/blog1.jpg"alt="image"><br>
+						<div class="desc"><h3>4 Schools together, 1 big celebration - AKTH </h3>The 4 schools are part of our Kishanbagh cluster. They are low-fee budget private schools serving the under-privileged communities of Kishanbagh area in Hyderabad.
+						</div>
+						<button type="button" class="btn btn-primary">READ MORE >></button>
+					</div>
+					<div class="gallery">
+						<img src="images/blog2.jpg"alt="image" ><br>
+						<div class="desc"><h3>Empowering Teachers Through Technology</h3>In our latest learning circle on 10th March'18, we decided to make our teachers come up with their own understanding with minimum lecture from us. With that aim in mind, we got tabs with internet connection and headphones for each teacher.
+						</div>
+						<button type="button" class="btn btn-primary">READ MORE >></button>
+					</div>
+					<div class="gallery">
+						<img src="images/blog3.jpg"alt="image" ><br>
+						<div class="desc"><h3>How it all began - The Journey of LOOP - Our Version </h3>Loop was started by Vikrant and Md Anwar in June 2016. Ashish and Saahil joined in as other two co-founders in the following year.In June 2016, they started working with other schools by training teachers and supporting school leaders.
+
+						</div>
+						<button type="button" class="btn btn-primary">READ MORE >></button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<footer class="footer-main-wrapper">
+			<div class="footer-links-wrapper">
+				<div class="container">
+					<div class="footer-links">
+						<h2>Contact Deatils</h2><br>
+						<ul>
+
+							<li>Loop Education Foundation</li>
+							<li>Hyderabad,Telangana</li>
+							<li>Call us: +91 8886171701</li>
+							<li class="special">Email:conatact@loopeducation.org</li>
+						</ul>
+					</div>
+				</div>
+					<div class="footer-links">
+						<h2 class="bottomright">Follow Us</h2><br>
+						<div class="bottomright">
+							<i class="fab fa-linkedin-in" ></i>
+							<i class="fab fa-facebook"></i>
+							<i class="fab fa-instagram"></i>
+						</div>
+
+					</div>
+			</div>
+			<div class="footer-copy">
+				<div class="container">
+					<p>Copyright &copy; Domain <span> TEAM 13<sapn></p>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
